571afcb86ab2e5587b74a2056d8d9aabcb220de0
rockysantiago <6001739+rockysantiago@users.noreply.github.com>
2022-07-24 22:30:49 +0800

    feat: update sb theme plugin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
-import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 import { ThemeProvider } from 'styled-components';
+import { withThemesProvider } from 'themeprovider-storybook';
+
 import { defaultTheme, GlobalStyle } from '../src/shared';
 
 addDecorator(withThemesProvider([{ ...defaultTheme }]), ThemeProvider);
