4d59abfa5b1751f01d5263236b006ae84b6d2af7
rockysantiago <6001739+rockysantiago@users.noreply.github.com>
2022-08-17 17:28:47 +0800

    fix test

--- src/components/PageLayout/PageLayout.test.tsx
+++ src/components/PageLayout/PageLayout.test.tsx
@@ -2,17 +2,20 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import { PageLayout } from '.';
+import { ProviderTest } from '../../tests';
 
 describe('PageLayout', () => {
   it('renders', () => {
     const { getByText } = render(
-      <PageLayout
-        header={'header'}
-        sider={{ isEnabled: true, node: 'sider' }}
-        footer={{ isEnabled: true, node: 'footer' }}
-      >
-        foo
-      </PageLayout>
+      <ProviderTest>
+        <PageLayout
+          header={'header'}
+          sider={{ isEnabled: true, node: 'sider' }}
+          footer={{ isEnabled: true, node: 'footer' }}
+        >
+          foo
+        </PageLayout>
+      </ProviderTest>
     );
     expect(getByText(/header/)).toBeInTheDocument();
     expect(getByText(/sider/)).toBeInTheDocument();
@@ -22,13 +25,15 @@ describe('PageLayout', () => {
 
   it('should not render sider and footer if isEnabled is false', () => {
     const { getByText, queryByText } = render(
-      <PageLayout
-        header={'header'}
-        sider={{ isEnabled: false, node: 'sider' }}
-        footer={{ isEnabled: false, node: 'footer' }}
-      >
-        bar
-      </PageLayout>
+      <ProviderTest>
+        <PageLayout
+          header={'header'}
+          sider={{ isEnabled: false, node: 'sider' }}
+          footer={{ isEnabled: false, node: 'footer' }}
+        >
+          bar
+        </PageLayout>
+      </ProviderTest>
     );
     expect(queryByText(/sider/)).not.toBeInTheDocument();
     expect(queryByText(/footer/)).not.toBeInTheDocument();
